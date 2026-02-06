c4cbe0e54389fba5f01b8d1ba678f5b18c0505ed
0xhwk <85083631+0xhwk@users.noreply.github.com>
2026-02-06 14:14:41 +0300

    Add Name and Symbol fields for ERC1155

--- MigratedERC1155Token.sol
+++ MigratedERC1155Token.sol
@@ -6,15 +6,22 @@ import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MigratedERC1155Token is ERC1155, Ownable {
+    string public name;
+    string public symbol;
+
     constructor(
         address initialOwner,
         string memory uri,
+        string memory _name,
+        string memory _symbol,
         uint256[] memory ids,
         uint256[] memory amounts
     )
         ERC1155(uri)
         Ownable(initialOwner)
     {
+        name = _name;
+        symbol = _symbol;
         require(ids.length == amounts.length, "IDs and amounts length mismatch");
 
         // Pre-mint all token IDs with specified amounts to owner
